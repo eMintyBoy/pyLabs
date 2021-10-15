7a4461897b8950bdbf24b1051cf014b32e20c94f
eMintyBoy <91306012+eMintyBoy@users.noreply.github.com>
2021-10-15 08:56:49 +0300

    Update lr5_zad1.py

--- Tanya/LR5/lr5_zad1.py
+++ Tanya/LR5/lr5_zad1.py
@@ -6,14 +6,18 @@ N = int(input("Введите количество элементов списк
 print("Введите элементы списка: ")
 
 for i in range(N):
-    a = float(input())
+    a = int(input())
     list.append(a)
 
 abs_min = abs(list[0])
 
 for i in range(len(list)):
-    if abs(list[i+1]) < abs_min:
+    if abs(list[i]) < abs_min:
         abs_min = abs(list[i+1])
 
 print("Минимальный по модулю элемент списка: ", abs_min)
-print(list.reverse)
+
+list.reverse()
+print("Список в обратном порядке", list)
+
+input()
